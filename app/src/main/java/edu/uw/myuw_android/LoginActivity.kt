84a1c7a1972d2ad1feb7e0fc1f6fad7a8b53d004
Sarthak Singh <ss269@uw.edu>
2020-05-27 12:05:32 -0700

    Moved work to onstart

--- app/src/main/java/edu/uw/myuw_android/LoginActivity.kt
+++ app/src/main/java/edu/uw/myuw_android/LoginActivity.kt
@@ -23,6 +23,10 @@ class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         loginButton.setOnClickListener {
             tryLoginWithAppAuth()
